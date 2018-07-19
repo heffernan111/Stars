6049deeb19b928ed901092043d6e469840a7c1a4
Unknown <david.heff.dh@googlemail.com>
2018-07-19 19:51:05 +0100

    SB Admin Theme

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('myHome');
+})->middleware('auth');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
