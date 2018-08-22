7ef9301922ce1c074c405c5f4dfd1f2115d22e73
Unknown <david.heff.dh@googlemail.com>
2018-08-22 20:47:45 +0100

    home page done
    starfield canvas

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -25,36 +25,15 @@
 
 </head>
 <body>
-<div class="flex-center position-ref full-height">
-    <div id="app">
-        @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                    <a href="/profile/{{ \Auth::user()->id }}" role="button">
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-                    @if($roles['roles'][0]['id'] == 1)
-                        <a href="{!! url('/admin'); !!}">Admin</a>
-                    @endif
-                    <a href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                        @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <div class="flex-center position-ref full-height">
+        <div id="container">
+            <div id="overlay">
+            
+            <main class="py-4">
+                @yield('content')
+            </main>
+            </div>
+        </div>
     </div>
-</div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,6 @@
                     <div class="title m-b-md">
                         Astro Gaze
                     </div>
-
                     <div class="links">
                         <a href="\guides">Guides</a>
                         <a href="\shop">Shop</a>
@@ -17,6 +16,30 @@
                         <a href="\maps">Maps</a>
                         <a href="\chat">Chat</a>
                     </div>
+                    @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                        <a href="/profile/{{ \Auth::user()->id }}" role="button">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        @if($roles['roles'][0]['id'] == 1)
+                            <a href="{!! url('/admin'); !!}">Admin</a>
+                        @endif
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                            @else
+                            <a href="{{ route('login') }}">Login</a>
+                            <a href="{{ route('register') }}">Register</a>
+                        @endauth
+                    </div>
+                @endif
                 </div>
         </div>
     </div>
